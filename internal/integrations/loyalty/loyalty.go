2340ca05e66695e3c74842375678d779950d9054
Зимин Николай <Nikola.Zimin@x5.ru>
2024-02-15 13:41:03 +0300

    try

--- internal/integrations/loyalty/loyalty.go
+++ internal/integrations/loyalty/loyalty.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"net/http"
 	"strconv"
+	"sync"
 	"time"
 
 	"github.com/zYoma/gophermart/internal/logger"
@@ -34,6 +35,9 @@ var (
 	ErrStatus     = errors.New("bad order status")
 	ErrStatusCode = errors.New("not success status")
 	ErrNotFound   = errors.New("order not found")
+	pauseMutex    sync.Mutex
+	pauseCond     = sync.NewCond(&pauseMutex)
+	isPaused      bool // вообще мне кажется, нужно использовать распределенное хранилище типа редиса для этого, если у нас несколько подов с приложением, но допустим, что у нас один процесс
 )
 
 // isValid проверяет, является ли статус заказа допустимым.
@@ -48,78 +52,75 @@ func (s OrderStatus) isValid() bool {
 
 func GetPointsByOrder(url string, pauseChan chan bool) (*OrderResponse, error) {
 	for {
-		select {
-		case <-pauseChan: // При получении сигнала о паузе, ожидаем следующий сигнал для продолжения
-			<-pauseChan
-		default: // Продолжаем выполнение, если пауза не активирована
-			resp, err := http.Get(url)
+		pauseMutex.Lock()
+		for isPaused { // Ждём, пока флаг паузы активен
+			pauseCond.Wait()
+		}
+		pauseMutex.Unlock()
+		resp, err := http.Get(url)
+		if err != nil {
+			logger.Log.Error("ошибка при выполнении запроса", zap.Error(err))
+			return nil, ErrRequest
+		}
+		// повторяем запрос при статусе 429
+		if resp.StatusCode == http.StatusTooManyRequests {
+			retryAfter := resp.Header.Get("Retry-After")
+			delaySeconds, err := strconv.Atoi(retryAfter)
 			if err != nil {
-				logger.Log.Error("ошибка при выполнении запроса", zap.Error(err))
-				return nil, ErrRequest
-			}
-			// повторяем запрос при статусе 429
-			if resp.StatusCode == http.StatusTooManyRequests {
-				retryAfter := resp.Header.Get("Retry-After")
-				delaySeconds, err := strconv.Atoi(retryAfter)
-				if err != nil {
-					logger.Log.Sugar().Infof("ошибка при чтении заголовка Retry-After", err)
-					resp.Body.Close()
-					return nil, ErrStatusCode
-				}
-				logger.Log.Sugar().Infof("Получен статус 429, повтор запроса через %d секунд\n", delaySeconds)
+				logger.Log.Sugar().Infof("ошибка при чтении заголовка Retry-After", err)
 				resp.Body.Close()
-				broadcastPause(pauseChan) // Отправляем сигнал о паузе всем горутинам
-				time.Sleep(time.Duration(delaySeconds) * time.Second)
-				broadcastResume(pauseChan) // Отправляем сигнал о возобновлении работы всем горутинам
-				continue
-			}
-
-			defer resp.Body.Close()
-
-			if resp.StatusCode != http.StatusOK {
-				// если заказ не найден
-				if resp.StatusCode == http.StatusNoContent {
-					logger.Log.Sugar().Infof("заказ не найден, url: %s", url)
-					return nil, ErrNotFound
-				}
-				logger.Log.Sugar().Infof("сервер вернул статус-код: %d, url: %s", resp.StatusCode, url)
 				return nil, ErrStatusCode
 			}
-
-			body, err := io.ReadAll(resp.Body)
-			if err != nil {
-				logger.Log.Error("ошибка при чтении тела ответа", zap.Error(err))
-				return nil, ErrReadBody
-			}
-
-			var orderResp OrderResponse
-			if err := json.Unmarshal(body, &orderResp); err != nil {
-				logger.Log.Error("ошибка при десериализации ответа", zap.Error(err))
-				return nil, ErrUnmarshal
-			}
-
-			if !orderResp.Status.isValid() {
-				logger.Log.Sugar().Infof("недопустимый статус заказа: %s, url: %s", orderResp.Status, url)
-				return nil, ErrStatus
-			}
-
-			return &orderResp, nil
+			logger.Log.Sugar().Infof("Получен статус 429, повтор запроса через %d секунд\n", delaySeconds)
+			resp.Body.Close()
+			activatePause()
+			time.Sleep(time.Duration(delaySeconds) * time.Second)
+			deactivatePause()
+			continue
 		}
+
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			// если заказ не найден
+			if resp.StatusCode == http.StatusNoContent {
+				logger.Log.Sugar().Infof("заказ не найден, url: %s", url)
+				return nil, ErrNotFound
+			}
+			logger.Log.Sugar().Infof("сервер вернул статус-код: %d, url: %s", resp.StatusCode, url)
+			return nil, ErrStatusCode
+		}
+
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			logger.Log.Error("ошибка при чтении тела ответа", zap.Error(err))
+			return nil, ErrReadBody
+		}
+
+		var orderResp OrderResponse
+		if err := json.Unmarshal(body, &orderResp); err != nil {
+			logger.Log.Error("ошибка при десериализации ответа", zap.Error(err))
+			return nil, ErrUnmarshal
+		}
+
+		if !orderResp.Status.isValid() {
+			logger.Log.Sugar().Infof("недопустимый статус заказа: %s, url: %s", orderResp.Status, url)
+			return nil, ErrStatus
+		}
+
+		return &orderResp, nil
 	}
 }
 
-func broadcastPause(pauseChan chan bool) {
-	// Отправляем сигнал о паузе, не блокируя, если канал уже заполнен
-	select {
-	case pauseChan <- true:
-	default:
-	}
+func activatePause() {
+	pauseMutex.Lock()
+	isPaused = true
+	pauseMutex.Unlock()
 }
 
-func broadcastResume(pauseChan chan bool) {
-	// Отправляем сигнал о возобновлении, не блокируя, если канал уже заполнен
-	select {
-	case pauseChan <- false:
-	default:
-	}
+func deactivatePause() {
+	pauseMutex.Lock()
+	isPaused = false
+	pauseCond.Broadcast() // Оповещаем все ожидающие горутины
+	pauseMutex.Unlock()
 }
